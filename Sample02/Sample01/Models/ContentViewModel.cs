2fb24ee9ef49ac499a24e40da07670373aace15c
Bobo <362510686@qq.com>
2018-12-19 18:55:21 +0800

    新增第三篇文章的事例代码

---
+++ Sample02/Sample01/Models/ContentViewModel.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace Sample01.Models
+{
+    /// <summary>
+    /// 2018.11.19
+    /// 祝雷
+    /// Content视图模式
+    /// </summary>
+    public class ContentViewModel
+    {
+        /// <summary>
+        /// 内容列表
+        /// </summary>
+        public List<Content> Contents { get; set; }
+    }
+}
